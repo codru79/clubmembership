180058ea386b1b3accf6312ef8ef3f26f742bf2f
codru79 <codrud@gmail.com>
2022-11-05 13:07:59 +0200

    employee

---
+++ clubmembership/ViewModels/CodeSnippetViewModelCreate.cs
@@ -0,0 +1,16 @@
+﻿using clubmembership.Models;
+using clubmembership.Repository;
+
+namespace clubmembership.ViewModels
+{
+    public class CodeSnippetViewModelCreate:CodeSnippetViewModel
+    {
+        public List<MemberModel> Members { get; set; }
+
+        public CodeSnippetViewModelCreate(CodeSnippetModel model, MemberRepository repository) :base(model,repository)
+        {
+            
+            this.Members = repository.GetAllMembers();
+        }
+    }
+}
